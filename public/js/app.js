3c98260294af6cf4eba02914f91fe23d1d3dc302
kiranharish <murugankiran2000@gmail.com>
2019-06-12 20:34:44 +0530

    fixed bug clearing the repo

---
+++ public/js/app.js
@@ -0,0 +1,28 @@
+const WeatherForm = document.querySelector('form')
+
+
+let msg_1 = document.querySelector('#msg_1')
+let msg_2 = document.querySelector('#msg_2')
+
+WeatherForm.addEventListener('submit',(e) =>{
+    e.preventDefault()
+    // const location = search.value
+
+    const location = document.querySelector('input').value
+
+    msg_1.textContent = "Loading...."
+    msg_2.textContent = " "
+
+    fetch('http://localhost:3000/weather?address='+location).then((response)=>{
+    response.json().then((data)=>{
+        if(data.error) msg_1.textContent = data.error
+        else{
+            msg_1.textContent = data.location
+            msg_2.textContent = data.forecast
+           
+        } 
+    })
+})
+
+
+})
